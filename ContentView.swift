a124f7d9930f0a0b464e329ab3b081ead6cb1c54
NathanKruchko <Kru512490@d204.lths.net>
2025-04-09 12:25:23 -0500

    Changes

--- ContentView.swift
+++ ContentView.swift
@@ -3,10 +3,12 @@ import SwiftUI
 struct ContentView: View {
     
     @State var answer = 3
-    //Did a thing with a thing 
+    @State var number1 = 0
+    @State var number2 = 0
+    
+    //Did a thing with a thing
     var body: some View {
-        @State var number1 = 0
-        @State var number2 = 0
+        
 
     }
 }
